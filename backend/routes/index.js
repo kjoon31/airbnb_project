f5742cb6477934266a4c372b4710b72d61da778e
daniel joonkyu kim <danielkim@daniels-mbp.lan>
2023-07-14 08:09:48 -0700

    review

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -49,7 +49,8 @@ router.post('/api/users', async ( req, res ) => {
 })
 router.post('/api/spots', async (req, res) => {
   const { address, city, state, country, lat, lng, name, description, price } = req.body;
-  // fetch
+  // fetch(ownerId)
+  const { ownerId } = req.body
   const spot = await Spot.create({
     // ownerId,
     address,
