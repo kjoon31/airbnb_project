3e409ed8172577ca7e1c19a1a873aa7a9c7e353b
daniel joonkyu kim <danielkim@daniels-mbp.lan>
2023-07-13 18:06:36 -0700

    users functionality

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,13 @@ const router = express.Router();
 // ...
 const apiRouter = require('./api');
 
+let bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
+
+
+const { User, Spot } = require('../db/models') 
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
@@ -19,7 +26,58 @@ router.get("/api/csrf/restore", (req, res) => {
   });
 });
 
-
+router.post('/api/users', async ( req, res ) => {
+  const { username, email, password } = req.body;
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword: bcrypt.hashSync(password, salt)
+  })
+  // console.log(user)
+  return res.json({
+    id: user.id,
+    firstName: null,
+    lastName: null,
+    email: user.email
+  })
+  // res.status(500)
+  /*
+  Successful response includes newly created id, firstName, lastName, and email
+  Error response with status 500 is given when the specified email or username already exists
+  Error response with status 400 is given when body validations for the email, firstName, or lastName are violated
+  */
+})
+router.post('/api/spots', async (req, res) => {
+  const { address, city, state, country, lat, lng, name, description, price } = req.body;
+  // fetch
+  const spot = await Spot.create({
+    // ownerId,
+    address,
+    city, 
+    state, 
+    country, 
+    lat, 
+    lng, 
+    name, 
+    description, 
+    price
+  })
+  return res.json({
+    id: spot.id,
+    ownerId: null, // the authenticated user's id
+    address: spot.address,
+    // city:, 
+    // state, 
+    // country, 
+    // lat, 
+    // lng, 
+    // name, 
+    // description, 
+    // price
+  })
+})
+  
+  
 
 // router.use('/api', apiRouter);
 // ...
